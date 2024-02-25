f7799e7f05707aafa9e765de82994f725b35af02
Sahani26 <sunilsahani484@gmail.com>
2024-02-25 19:30:13 +0530

    add updata delete

--- server.js
+++ server.js
@@ -2,25 +2,116 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port =  3000
-   
+const port = 3000
 
+const dataArray = require('./students');
 
+app.use(express.json()) //post k phle chlata hai
+
+app.get('/r', (req, res) => {
+  res.send('Hello World!dfg')
+})
 app.get('/', (req, res) => {
-    res.send('Hello World!dfg')
+  res.send(dataArray);
+});
+
+
+app.get("/test/:id", (req, res) => {
+  const id = req.params.id
+  res.status(200).json({ name: `hello name is world ${id}` })
+
+})
+
+app.post('/post', (req, res) => {
+
+
+
+  if (!req.body.email) {
+    res.status(400)
+    return res.json({ error: "email reqire" })
+  }
+  const user = {
+    id: dataArray.length + 1,
+    first_name: req.body.first_name,
+    email: req.body.email,
+  }
+  dataArray.push(user)
+  res.json(user);
+
+})
+
+
+// app.put('/put/:id', (req, res) => {
+//   let id = req.params.id
+//   let first_name = req.body.first_name
+//   let email = req.body.email
+//   let index = students.findIndex((dataArray) => {
+//     return (dataArray.id == Number.parseInt(id))
+//   })
+//   if (index >= 0) {
+//     let std = students[index]
+//     std.first_name = first_name
+//     std.email = email
+//     res.json(std)
+//   }
+//   else {
+//     res.status(400)
+//   }
+
+// })
+app.put('/put/:id', (req, res) => {
+  // 65 66 for read previous data request.body is udr for data read
+  const id = req.params.id
+  let first_name = req.body.first_name
+  let email = req.body.email
+  1 //perticular  array ke searching ke liye findindex use krte hai auer esme ek element pass krte hai
+
+  let index = dataArray.findIndex((students) => {
+    return (students.id == Number.parseInt(id))
   })
-  
-  app.get('/test', (req, res) => {
-    // res.send('Hello test World!')
-    res.status(200).json({name:"sunil" , mobile:"9596"})
-  
+  //Number.parseInt integer m bdlne k liye
+
+  if (index >= 0) {
+    let std = dataArray[index]
+    std.first_name = first_name
+    std.email = email
+    res.json(std)
+
+    //res.json(std) jo update hai usme send krne k liye nya bhejna
+  }
+  else {
+    res.status(400)
+  }
+
+
+})
+
+
+app.delete('/delete/:id', (req , res)=>{
+  const id = req.params.id
+  let index = dataArray.findIndex((students) => {
+    return (students.id == Number.parseInt(id))
   })
 
-  app.get("/test/:id",(req, res)=>{
-    const id = req.params.id
-    res.status(200).json({name:`hello name is world ${id}`})
 
-  })
+  
+  if (index >= 0) {
+    let std = dataArray[index]
+    dataArray.splice(index , 1)
+    res.json(std)
+
+    //res.json(std) jo update hai usme send krne k liye nya bhejna
+  }
+  else {
+    res.status(400)
+    res.end()
+  }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
---
+++ students.js
@@ -0,0 +1,43 @@
+const dataArray = [{
+    "id": 1,
+    "first_name": "Valina",
+    "email": "vzuker0@google.de"
+  }, {
+    "id": 2,
+    "first_name": "Danni",
+    "email": "dcurrao1@webnode.com"
+  }, {
+    "id": 3,
+    "first_name": "Josiah",
+    "email": "jgeane2@google.co.jp"
+  }, {
+    "id": 4,
+    "first_name": "Carilyn",
+    "email": "ckinton3@oaic.gov.au"
+  }, {
+    "id": 5,
+    "first_name": "Kip",
+    "email": "kbruna4@cargocollective.com"
+  }, {
+    "id": 6,
+    "first_name": "Angus",
+    "email": "arollings5@bloglines.com"
+  }, {
+    "id": 7,
+    "first_name": "Fleur",
+    "email": "fales6@bloglines.com"
+  }, {
+    "id": 8,
+    "first_name": "Jorry",
+    "email": "jdomsalla7@bing.com"
+  }, {
+    "id": 9,
+    "first_name": "Adamo",
+    "email": "aselwood8@nature.com"
+  }, {
+    "id": 10,
+    "first_name": "Daile",
+    "email": "dpaterson9@wordpress.com"
+  }];
+module.exports = dataArray;
+
